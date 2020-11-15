faac4ab5027844623e8103bdb369a91b00e3d373
Hossam Omar <HossamMuhammedOmar>
2020-11-15 04:13:57 -0800

    back to project

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
-import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,8 +9,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  final List<String> imageList = [];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,40 +17,13 @@ class HomeScreenState extends State<HomeScreen> {
       ),
       drawer: MyDrawer(),
       body: Center(
-        child: imageList.length > 0
-            ? Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: ListView.builder(
-                  // itemCount: imageList.length,
-                  itemBuilder: (context, index) {
-                    return _buildImage(imageList[index]);
-                  },
-                ),
-              )
-            : CircularProgressIndicator(),
-      ),
+          child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+      )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => _addImage(),
+        onPressed: () => () {},
       ),
     );
-  }
-
-  Widget _buildImage(String image) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 10.0),
-      child: Card(
-        child: Image.network(image),
-      ),
-    );
-  }
-
-  _addImage() async {
-    final response =
-        await http.get('https://jsonplaceholder.typicode.com/photos/1');
-
-    final parsedResponse = jsonDecode(response.body);
-    imageList.add(parsedResponse['url']);
-    print(imageList.length);
   }
 }
